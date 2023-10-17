2b7bf928a96bd8914755cbe195df3d1269a7c903
OscarBallart <terrakkeo@hotmail.com>
2023-10-17 17:12:28 +0200

    exemple6 6ª MODIF (git - Netbeans)

--- src/main/java/m13/projectgitnetbeans/ProjectGitNetbeans.java
+++ src/main/java/m13/projectgitnetbeans/ProjectGitNetbeans.java
@@ -3,8 +3,12 @@ package m13.projectgitnetbeans;
 import m13.projectgitnetbeans.Salutacio_v1;
 
 /**
+ * https://www.youtube.com/playlist?list=PLQxX2eiEaqby-qh4raiKfYyb4T7WyHsfW
+ * https://www.freecodecamp.org/espanol/news/10-comandos-de-git-que-todo-desarrollador-deberia-saber/
  * https://youtu.be/q6RKq91FKC4?si=xLXU3hoyH1nrjp8i
  * https://youtu.be/NXn87ID7TKQ?si=hXP_2CSR2X3SPNTa
+ * https://youtu.be/A0r50ZrRN2k?si=w-R72oWyquu1qNvP
+ * https://youtu.be/G_zKrG2mkrg?si=JN7xT_aI-p37S2nI
  *
  * @author Oscar
  */
--- src/main/java/m13/projectgitnetbeans/Salutacio_v1.java
+++ src/main/java/m13/projectgitnetbeans/Salutacio_v1.java
@@ -2,8 +2,12 @@
 package m13.projectgitnetbeans;
 
 /**
+ * https://www.youtube.com/playlist?list=PLQxX2eiEaqby-qh4raiKfYyb4T7WyHsfW
+ * https://www.freecodecamp.org/espanol/news/10-comandos-de-git-que-todo-desarrollador-deberia-saber/
  * https://youtu.be/q6RKq91FKC4?si=xLXU3hoyH1nrjp8i
  * https://youtu.be/NXn87ID7TKQ?si=hXP_2CSR2X3SPNTa
+ * https://youtu.be/A0r50ZrRN2k?si=w-R72oWyquu1qNvP
+ * https://youtu.be/G_zKrG2mkrg?si=JN7xT_aI-p37S2nI
  *
  * @author Oscar
  */
@@ -15,5 +19,6 @@ public class Salutacio_v1 {
       System.out.println("Nou video");
       System.out.println("Regeneracio del token a GIT");
       System.out.println("altres");
+      System.out.println("");
    }
 }
