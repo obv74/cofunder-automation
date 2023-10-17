a2b6a7ba0e4336a4231656dce5a5fc7fb17ae709
OscarBallart <terrakkeo@hotmail.com>
2023-10-17 12:16:26 +0200

    exemple 2ª MODIF afegim nova classe (git - Netbeans)

--- src/main/java/m13/projectgitnetbeans/ProjectGitNetbeans.java
+++ src/main/java/m13/projectgitnetbeans/ProjectGitNetbeans.java
@@ -1,17 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package m13.projectgitnetbeans;
+
+import m13.projectgitnetbeans.Salutacio;
 
 /**
  * https://youtu.be/q6RKq91FKC4?si=xLXU3hoyH1nrjp8i
+ *
  * @author Oscar
  */
 public class ProjectGitNetbeans {
 
-    public static void main(String[] args) {
-        System.out.println("Hola Mon");
-        System.out.println("Proves GIT-NETBEANS");
-    }
+   public static void main(String[] args) {
+      new Salutacio().saludar();
+   }
+
 }
---
+++ src/main/java/m13/projectgitnetbeans/Salutacio.java
@@ -0,0 +1,14 @@
+
+package m13.projectgitnetbeans;
+
+/**
+ *
+ * @author Oscar
+ */
+public class Salutacio {
+
+   public void saludar() {
+      System.out.println("Hola Mon");
+      System.out.println("Proves GIT-NETBEANS");
+   }
+}
