9b407c7f319d61bdbb2dc4f4c8ff21243d98441f
OscarBallart <terrakkeo@hotmail.com>
2023-10-17 11:38:34 +0200

    exemple MODIFICACIO arxiu (git - Netbeans)

--- src/main/java/m13/projectgitnetbeans/ProjectGitNetbeans.java
+++ src/main/java/m13/projectgitnetbeans/ProjectGitNetbeans.java
@@ -5,12 +5,13 @@
 package m13.projectgitnetbeans;
 
 /**
- *
+ * https://youtu.be/q6RKq91FKC4?si=xLXU3hoyH1nrjp8i
  * @author Oscar
  */
 public class ProjectGitNetbeans {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola Mon");
+        System.out.println("Proves GIT-NETBEANS");
     }
 }
