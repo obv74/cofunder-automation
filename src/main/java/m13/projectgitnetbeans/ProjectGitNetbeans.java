c59fc27fa4154ef5e9c36b6dd75ad8c3f6592890
OscarBallart <terrakkeo@hotmail.com>
2023-10-17 11:29:34 +0200

    exemple us git - Netbeans

---
+++ src/main/java/m13/projectgitnetbeans/ProjectGitNetbeans.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package m13.projectgitnetbeans;
+
+/**
+ *
+ * @author Oscar
+ */
+public class ProjectGitNetbeans {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
